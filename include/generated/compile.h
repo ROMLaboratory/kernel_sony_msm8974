52404c0337563e84762f19c9891e0eef9bac4fd8
kiko <jfdsmabalot@gmail.com>
2014-02-18 17:56:37 +0800

    Merge Sony Sources

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 SMP PREEMPT Tue Feb 18 17:56:01 PHT 2014"
+#define LINUX_COMPILE_BY "gnome"
+#define LINUX_COMPILE_HOST "jfdsmabalot-gnu"
+#define LINUX_COMPILER "gcc version 4.8.3 20140124 (release) (SM-Toolchain) "
---
+++ usr/.initramfs_data.cpio.d
@@ -0,0 +1,1 @@
+deps_initramfs := /home/gnome/M40/kernel/sony/z1/scripts/gen_initramfs_list.sh \
